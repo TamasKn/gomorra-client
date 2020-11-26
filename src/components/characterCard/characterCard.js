3f4c2aaf79c89a5424d33bb1ca2f70fab86957a3
tx <tamas.knisz@gmail.com>
2020-11-26 15:30:14 +0100

    Character card added, Home page updated

---
+++ src/components/characterCard/characterCard.js
@@ -0,0 +1,51 @@
+import {useState} from 'react'
+import '../../css/characterCard.css'
+
+const CharacterCard = ({image, name, nickname, occupation, clan, actor}) => {
+
+    const [cardHover, setCardHover] = useState(false)
+
+    const nameFormat = (name, nickname) => {
+        if(!name) {
+            return <h2>" {nickname} "</h2>
+        } else if(!nickname) {
+            return <h2>{name}</h2>
+        } else {
+            return(
+                <>
+                    <h2>{name}</h2>
+                    <h3>" {nickname} "</h3>
+                </>
+            )
+        }
+    }
+
+    return(
+        <div className="card--container"
+             onMouseEnter={() => setCardHover(true)}
+             onMouseLeave={() => setCardHover(false)}
+        >
+
+            <div className="card--image"
+                 style={{backgroundImage: `url(${image})`}}
+            />
+
+            <div className="card--description"
+                style={(cardHover) ? {height: '34%'} : {height: '17%'}}
+            >
+                <div className="card--description-name">
+                    {nameFormat(name, nickname)}
+                </div>
+                <div className="card--description-infos">
+                    <p>Occupation: {occupation}</p>
+                    <p>Clan: {clan}</p>
+                    <p>Portrayed by: {actor}</p>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default CharacterCard
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,42 @@
+import {useState, useEffect} from 'react'
+import CharacterCard from "../../components/characterCard/characterCard"
+import '../../css/home.css'
+import axios from 'axios'
 
 const Home = () => {
 
+    const [characters, setCharacters] = useState([])
+
+    useEffect(() => {
+        async function fetchData() {
+            const resp = await axios.get("https://gomorra-api.herokuapp.com/characters/placeholder/")
+            setCharacters(resp.data)
+        }
+
+        fetchData()
+
+    }, [])
+
     return (
         <div className="content-wrapper">
-            <h1>Content</h1>
+            <section className="home__container">
+                {
+                    (characters.length !== 0)
+                    ?
+                        characters.data.map( el => (
+                            <CharacterCard
+                                key={el.id}
+                                name={el.name}
+                                nickname={el.nickname}
+                                occupation={el.occupation}
+                                clan={el.clan}
+                                actor={el.actor}
+                                image={el.picture} />
+                        ))
+                    : <p>Loading...</p>
+                }
+
+            </section>
         </div>
     );
 }
