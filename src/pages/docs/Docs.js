b5238fd75924706b879452dd16fa3135f05b6204
tx <tamas.knisz@gmail.com>
2020-11-30 13:52:51 +0100

    Documentation page

--- src/pages/docs/Docs.js
+++ src/pages/docs/Docs.js
@@ -12,6 +12,7 @@ const Documentation = () => {
     })
 
     // Function that reads code block sample from local file
+    // Wanted to avoid installing 3rd party library for code block display
     async function readLocalFile(file) {
         const resp = await fetch(file)
         const data = await resp.text()
@@ -25,6 +26,10 @@ const Documentation = () => {
         const divs = document.getElementsByClassName('code__multi-line')
         divs[0].innerHTML = await readLocalFile(blocks.cb1).then(res => res)
         divs[1].innerHTML = await readLocalFile(blocks.cb2).then(res => res)
+        divs[2].innerHTML = await readLocalFile(blocks.cb3).then(res => res)
+        divs[3].innerHTML = await readLocalFile(blocks.cb4).then(res => res)
+        divs[4].innerHTML = await readLocalFile(blocks.cb5).then(res => res)
+        divs[5].innerHTML = await readLocalFile(blocks.cb6).then(res => res)
     }
 
 
@@ -40,15 +45,15 @@ const Documentation = () => {
                 <div className="docs__sidebar">
                     <ul>
                         <li><a href="#section-1">Base URI</a></li>
-                        <li><a href="#section-1">Endpoints</a></li>
+                        <li><a href="#section-2">Endpoints</a></li>
                         <li className="docs__sidebar--sublist">
-                            <a href="#section-2">Characters</a>
+                            <a href="#section-2.1">Characters</a>
                         </li>
                         <li className="docs__sidebar--sublist">
-                            <a href="#section-2">Deaths</a>
+                            <a href="#section-2.2">Deaths</a>
                         </li>
                         <li className="docs__sidebar--sublist">
-                            <a href="#section-2">Episodes</a>
+                            <a href="#section-2.3">Episodes</a>
                         </li>
                         <li><a href="#section-3">Usage</a></li>
                         <li><a href="#section-4">Install locally</a></li>
@@ -57,62 +62,181 @@ const Documentation = () => {
 
                 <main>
                     <section id="section-1">
-                        <h1>Section 1</h1>
+                        <h1>Base URI</h1>
+                        <hr/>
 
-                        <SingleLine>{" function() { return } "}</SingleLine>
-
-                        <MultiLines />
-                        <MultiLines />
+                        <SingleLine>{"https://gomorra-api.herokuapp.com/"}</SingleLine>
 
                     </section>
+
                     <section id="section-2">
-                        <h1>Section 2</h1>
-                        <p>Ratione nulla nam, ipsa dignissimos corrupti veniam nostrum, laudantium asperiores sequi
-                            numquam placeat velit voluptate in praesentium non labore unde incidunt laborum maxime quae
-                            magni.</p>
+                        <h1>Endpoints</h1>
+                        <hr/>
+
+                        <SingleLine>{"/characters"}</SingleLine>
+                        <SingleLine>{"/deaths"}</SingleLine>
+                        <SingleLine>{"/episodes"}</SingleLine>
+
                     </section>
+
+                    <section id="section-2.1">
+                        <h2>Characters</h2>
+                        <br/>
+
+                        <table border="1">
+                            <tr>
+                                <th>Attribute</th>
+                                <th>Type</th>
+                                <th>Description</th>
+                            </tr>
+                            <tr><td>id</td><td>integer</td><td>Unique ID for each character</td></tr>
+                            <tr><td>name</td><td>string</td><td>Formal name of the character</td></tr>
+                            <tr><td>nickname</td><td>string</td><td>Nickname, pet name of a character</td></tr>
+                            <tr><td>occupation</td><td>string</td><td>Job title or position in a clan/gang</td></tr>
+                            <tr><td>clan</td><td>string</td><td>Name of the clan(s) what the character has relation</td></tr>
+                            <tr><td>picture</td><td>string</td><td>Image URI of character</td></tr>
+                            <tr><td>status</td><td>string</td><td>Character alive or dead Alive or Deceased</td></tr>
+                            <tr><td>seasons</td><td>array</td><td>List of integer which season has the character appearance</td></tr>
+                            <tr><td>actor</td><td>string</td><td>Name of the actor/actress the character portrayed by</td></tr>
+                        </table>
+                        <br/><br/>
+
+                        <h3>Get all characters</h3>
+                        <SingleLine>{"/characters"}</SingleLine>
+
+                        <h3>Characters placeholder. Returns 10 random characters. Use as a placeholder in development project</h3>
+                        <SingleLine>{"/characters/placeholder"}</SingleLine>
+
+                        <h3>Get single character by ID</h3>
+                        <SingleLine>{"/characters/id/1"}</SingleLine>
+                        <MultiLines />
+
+                        <h3>Get character(s) by name. It will search in both name and nickname</h3>
+                        <SingleLine>{"/characters/name/track"}</SingleLine>
+                        <MultiLines />
+
+                        <h3>Get character(s) by their clan name</h3>
+                        <SingleLine>{"/characters/clan/conte"}</SingleLine>
+
+                        <h3>Get character(s) by their status - accepts only
+                            &nbsp;<span className="inline">alive</span> or
+                            &nbsp;<span className="inline">deceased</span> keywords
+                        </h3>
+                        <SingleLine>{"/characters/status/alive"}</SingleLine>
+
+                        <h3>Get characters by which season had appearance</h3>
+                        <SingleLine>{"/characters/season/2"}</SingleLine>
+
+
+                    </section>
+                    <section id="section-2.2">
+                        <h2>Deaths</h2>
+                        <br/>
+
+                        <table border="1">
+                            <tr>
+                                <th>Attribute</th>
+                                <th>Type</th>
+                                <th>Description</th>
+                            </tr>
+                            <tr><td>id</td><td>integer</td><td>Unique ID for each death event</td></tr>
+                            <tr><td>victim</td><td>string</td><td>Name or nickname of the character has been deceased</td></tr>
+                            <tr><td>cause</td><td>string</td><td>Reason of death</td></tr>
+                            <tr><td>perpetrator</td><td>string</td><td>Name or nickname of the the character who caused directly or indirectly</td></tr>
+                            <tr><td>body_count</td><td>integer</td><td>Number of dead happened during the event</td></tr>
+                            <tr><td>season</td><td>integer</td><td>Season number where the event occured</td></tr>
+                            <tr><td>episode</td><td>integer</td><td>Episode number where the event occured</td></tr>
+                        </table>
+                        <br/><br/>
+
+                        <h3>Get all deaths</h3>
+                        <SingleLine>{"/deaths"}</SingleLine>
+
+                        <h3>Get certain death by ID</h3>
+                        <SingleLine>{"/deaths/id/56"}</SingleLine>
+                        <MultiLines />
+
+                        <h3>Get death(s) by the victim's name or nickname</h3>
+                        <SingleLine>{"/deaths/victim/capae"}</SingleLine>
+                        <MultiLines />
+
+                        <h3>Get death(s) by perpetrator's name or nickname</h3>
+                        <SingleLine>{"/deaths/perpetrator/pietro"}</SingleLine>
+
+                        <h3>Get death(s) by certain season</h3>
+                        <SingleLine>{"/deaths/season/4"}</SingleLine>
+
+                        <h3>Get death(s) by the season and episode number</h3>
+                        <SingleLine>{"/deaths/season/4/episode/9"}</SingleLine>
+
+                    </section>
+
+                    <section id="section-2.3">
+                        <h2>Episodes</h2>
+                        <br/>
+
+                        <table border="1">
+                            <tr>
+                                <th>Attribute</th>
+                                <th>Type</th>
+                                <th>Description</th>
+                            </tr>
+                            <tr><td>id</td><td>integer</td><td>Unique ID for each episodes</td></tr>
+                            <tr><td>title</td><td>string</td><td>Title of the episode</td></tr>
+                            <tr><td>season</td><td>integer</td><td>Season number</td></tr>
+                            <tr><td>episode</td><td>integer</td><td>Episode numberd</td></tr>
+                        </table>
+                        <br/><br/>
+
+                        <h3>Get all episodes</h3>
+                        <SingleLine>{"/episodes"}</SingleLine>
+
+                        <h3>Get episodes by season number</h3>
+                        <SingleLine>{"/episodes/season/3"}</SingleLine>
+
+                        <h3>Get episode by the season and episode number</h3>
+                        <SingleLine>{"/episodes/season/2/episode/9"}</SingleLine>
+                        <MultiLines />
+
+                        <h3>Get episode by its title</h3>
+                        <SingleLine>{"/episodes/title/uccidere"}</SingleLine>
+                        <MultiLines />
+
+                    </section>
+
                     <section id="section-3">
-                        <h1>Section 3</h1>
-                        <p>Soluta quibusdam ad nostrum vel voluptate delectus sequi dolores quia quaerat officia
-                            corrupti, aperiam fugit facere debitis repudiandae praesentium sapiente inventore
-                            repellendus, nemo commodi alias!</p>
+                        <h1>Usage and contribution</h1>
+                        <hr/><br/>
+                        <p>
+                            The API is free to use with the limit of 3000 requests/day.
+                            If exceeds it will send the 429 status code.
+                        </p>
+                        <p>
+                            Feel free to make Pull Request if you like to add or correct content.
+                        </p>
+                        <br/>
+
+                        <h3>Error fix</h3>
+                        <div className="docs__order">
+                            <p>1. Open a new issue</p>
+                            <p>2. Assign yourself</p>
+                            <p>3. Include the issue number in the PR</p>
+                            <p>4. Make the Pull request</p>
+                        </div>
+
                     </section>
                     <section id="section-4">
-                        <h1>Section 4</h1>
-                        <p>Aliquid aliquam magnam ducimus similique obcaecati, unde exercitationem laborum incidunt,
-                            quas in ipsum inventore nostrum? Blanditiis optio cumque earum iste odio! Alias sint
-                            accusamus repudiandae.</p>
+                        <h1>Install locally</h1>
+                        <hr/><br/>
+
+                        <div className="docs__order">
+                            <p>1. <span className="inline">npm install</span></p>
+                            <p>2. <span className="inline">npm run dev</span></p>
+                            <p>3. Open <span className="inline">localhost:4000</span></p>
+                        </div>
+
                     </section>
-                    <section id="section-5">
-                        <h1>Section 5</h1>
-                        <p>Officia ipsum fugit iure eaque quisquam error tempore earum enim illum, delectus officiis
-                            incidunt corrupti aliquid nam quas perspiciatis eveniet doloremque quod labore? Doloremque,
-                            ipsum?</p>
-                    </section>
-                    <section id="section-6">
-                        <h1>Section 6</h1>
-                        <p>Aperiam repellat dignissimos fugiat possimus esse, suscipit neque nisi libero alias obcaecati
-                            ipsam, porro illo corrupti nostrum reprehenderit unde, illum in laudantium impedit. Modi,
-                            veniam.</p>
-                    </section>
-                    <section id="section-7">
-                        <h1>Section 7</h1>
-                        <p>Cum asperiores temporibus itaque consequatur quod inventore, quia quis explicabo dicta esse
-                            minus voluptatem reiciendis eveniet animi, necessitatibus illum dolorem doloremque repellat
-                            placeat, dolores eaque.</p>
-                    </section>
-                    <section id="section-8">
-                        <h1>Section 8</h1>
-                        <p>Optio qui, omnis itaque rerum iusto molestiae necessitatibus deleniti quod tenetur id
-                            perspiciatis voluptatum dolorum quisquam eius ipsum non architecto labore! Distinctio,
-                            tenetur. Officiis, necessitatibus?</p>
-                    </section>
-                    <section id="section-9">
-                        <h1>Section 9</h1>
-                        <p>Rem iste iure blanditiis excepturi esse nisi corrupti sequi, illo, laborum quo quis quaerat
-                            assumenda perspiciatis quod fuga vel laudantium doloribus architecto tempora omnis
-                            earum!</p>
-                    </section>
+
                 </main>
             </div>
         </div>
--- src/static/code_blocks/FileIndexes.js
+++ src/static/code_blocks/FileIndexes.js
@@ -1,9 +1,17 @@
 import cb1 from './cb1.txt'
 import cb2 from './cb2.txt'
+import cb3 from './cb3.txt'
+import cb4 from './cb4.txt'
+import cb5 from './cb5.txt'
+import cb6 from './cb6.txt'
 
 const blocks = {
     cb1: cb1,
-    cb2: cb2
+    cb2: cb2,
+    cb3: cb3,
+    cb4: cb4,
+    cb5: cb5,
+    cb6: cb6,
 }
 
 export default blocks
